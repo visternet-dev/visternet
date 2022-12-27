5a393c95ebb27da3342a5c61509265c09e150ad0
mrbadri <mohammad.b@arianalabs.io>
2022-12-27 16:28:14 +0330

    first commit

---
+++ src/layout/LandingLayout/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LandingLayout({ children }) {
+  return <div>{children}</div>;
+}
+
+export default LandingLayout;
---
+++ src/layout/index.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+
+// project import
+import LAYOUT from 'constant';
+import MainLayout from './MainLayout';
+import MinimalLayout from './MinimalLayout';
+import AuthGuard from 'utils/route-guard/AuthGuard';
+import GuestGuard from 'utils/route-guard/GuestGuard';
+import LandingLayout from './LandingLayout';
+
+// ==============================|| LAYOUTS - STRUCTURE ||============================== //
+
+export default function Layout({ variant = LAYOUT.main, children }) {
+  switch (variant) {
+    case LAYOUT.minimal:
+      return <MinimalLayout>{children}</MinimalLayout>;
+
+    case LAYOUT.noauth:
+      return (
+        <GuestGuard>
+          <MinimalLayout>{children}</MinimalLayout>
+        </GuestGuard>
+      );
+
+    default:
+      return <LandingLayout>{children}</LandingLayout>;
+  }
+}
+
+//     return (
+//       <AuthGuard>
+//         <MainLayout>{children}</MainLayout>
+//       </AuthGuard>
+//     );
+// }
+
+
+// Layout.propTypes = {
+//   children: PropTypes.node,
+//   variant: PropTypes.string
+// };
